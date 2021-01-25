9a5b19cae2465a9dbaf79c726994c1b4d7bde239
TigranP89 <62433823+TigranP89@users.noreply.github.com>
2021-01-25 21:05:48 +0400

    Added timer

--- click_me/script.js
+++ click_me/script.js
@@ -1,15 +1,26 @@
-
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 var buttonCli = document.querySelector('.buttonCli');
 
 
+
+
+
+var i = 1;
+var d = new Date;
 buttonCli.onclick = function(){
 
-    document.querySelector('.buttonCli').style.top = getRndInteger(0, 1201) + "px";
-    document.querySelector('.buttonCli').style.left = getRndInteger(0, 1201) + "px";
-   
+  document.querySelector('.buttonCli').style.top = getRndInteger(0, 1201) + "px";
+  document.querySelector('.buttonCli').style.left = getRndInteger(0, 1201) + "px";
+  
+  buttonCli.innerHTML = 'Click Me ' + i;
+  i++;
 
+  if(i == 12){
+var d1 = new Date - d;
+alert("Вам понадобилось " + d1/1000 + " секунд");
+
+}
 }
